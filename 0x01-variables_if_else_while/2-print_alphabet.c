16f2cc86e8f749f32a5f41da7a15af43332fd5c5
kingsleyani <anikingsdc@gmail.com>
2023-03-20 02:03:35 -0700

    the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Print the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alph[26] = "abcdefghijklmnopqestuvwxyz";
+	int i;
+
+	for (i = a; i < 26; i++)
+	{
+		putchar(alph[1]);
+	}
+	putchar('\n');
+	return (0);
+}
