06e6f28f886d62c762fc9cc22984f844c4763cd6
kingsleyani <anikingsdc@gmail.com>
2023-03-26 05:08:50 -0700

    print lowercase of the alphabet 10 time

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,22 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 check code
- * Description - function uses putchar function to print
+ * print_alphabet_x10 - Check holberton
+ * Description: function uses _putchar function to print
  * alphabet in lowercase 10 times
+ * Return: Nothing.
  */
+
 void print_alphabet_x10(void)
 {
 	char i;
 	int j;
 
-	for (j = 0; j <= 10; j++)
+	for (j = 0; j < 10; j++)
 	{
 		for (i = 'a'; i <= 'z'; i++)
 			_putchar(i);
 		_putchar('\n');
 	}
+
 }
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase character
+ * @c : the character to be checked
+ * Return: 1 if c is lowercase 0 otherwise
+ */
+int _islower(int c)
+{
+	char i;
+	int lower = 0;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == c)
+		lower = 1;
+	}
+	return (lower);
+}
