1ea93360fd8cc5198c50fca75b351cea4064020d
kingsleyani <anikingsdc@gmail.com>
2023-04-19 02:09:08 -0700

    Alx task 0x08. C - Recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,27 +1,18 @@
 #include "main.h"
-
 /**
- * _puts_recursion - prints a string, followed by a new line.
- * @s: string
- * Return: no return.
+ * _puts_recursion - function like puts();
+ * @s: input
+ * Return: Always 0 (Success)
  */
 void _puts_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s)
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 	else
 		_putchar('\n');
-=======
-	_putchar(*s);
-	_puts_recursion(s + 1);
->>>>>>> 74885f06e81c2ca5cf628d76b34f131b2e6ffb8a
-=======
-	else
-		_putchar('\n');
->>>>>>> 3f5bac06041cc6e9678af7c1b627d754071edbf8
 }
+
