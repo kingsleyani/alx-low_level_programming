7189f2b7b856c84dd7842ef73701a2c1989d9caa
kingsleyani <anikingsdc@gmail.com>
2023-05-03 16:06:42 -0700

    deleting

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * pop_listint - Deleting the head node of a linked list
+ * @head: Address of the head of the list
+ *
+ * Return: Data of the head deleted
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *pointa;
+	int n;
+
+	if (*head == NULL)
+		return (0);
+
+	temp = *head;
+	n = pointa->n;
+	*head = pointa->next;
+	free(pointa);
+	return (n);
+
