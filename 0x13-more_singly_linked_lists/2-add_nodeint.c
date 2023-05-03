fd8914ae6f86c3aa26d9771b961dd0c611598583
kingsleyani <anikingsdc@gmail.com>
2023-05-03 15:11:23 -0700

    node adding

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -26,6 +26,6 @@ listint_t *add_nodeint(listint_t **head, const int n)
 		*head = temp;
 	}
 
-	return(*head);
+	return (*head);
 
 }
--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -29,7 +29,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		while (t->next != NULL)
 			t = t->next;
 
-		t->next = temp
+		t->next = temp;
 	}
 
 
